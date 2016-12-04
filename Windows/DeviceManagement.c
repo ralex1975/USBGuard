e0b8fc50cd19903b3ae2d75189371244cabfae79
landhb <landherrb@gmail.com>
2016-12-03 22:22:24 -0500

    Fixed overflow bug

--- Windows/DeviceManagement.c
+++ Windows/DeviceManagement.c
@@ -24,10 +24,10 @@ int SetRegistryValue(DWORD value, wchar_t * path, int path_size, wchar_t * varia
 
 	HKEY key;
 
-	wchar_t PATH_V[path_size];
+	wchar_t PATH_V[path_size+1];
 	wcsncpy(PATH_V, path, path_size);
 
-	wchar_t VAR_V[var_size];
+	wchar_t VAR_V[var_size+1];
 	wcsncpy(VAR_V, variable, var_size);
 
 	//wprintf(L"%s\n", PATH_V);
@@ -59,6 +59,11 @@ int DisableUSBDevices() {
 
 	SetRegistryValue(number, USBSTOR_BASE, 44, L"Start", 5);
 
+	number = 0x00000000;
+
+	SetRegistryValue(number, USBSTOR_ENUM, 50, L"Count", 5);
+	SetRegistryValue(number, USBSTOR_ENUM, 50, L"NextInstance", 13);
+
 	return 0;
     
 }
@@ -67,6 +72,11 @@ int EnableUSBDevices() {
 	DWORD number = 0x00000003;
 
 	SetRegistryValue(number, USBSTOR_BASE, 44, L"Start", 5);
+
+	number = 0x00000000;
+
+	SetRegistryValue(number, USBSTOR_ENUM, 50, L"Count", 5);
+	SetRegistryValue(number, USBSTOR_ENUM, 50, L"NextInstance", 13);
 
 	return 0;
 }
